15870310c400c317e88729839a8c7872aa6316d9
neha89 <kamini.er@gmail.com>
2023-08-18 11:57:42 +0530

    Created first component"ExpenseItem"

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem(){
+    return (
+        <>
+            <h2>Expense Items</h2>
+            <p>Food Rs 10</p>
+            <p>Petrol Rs 100</p>
+            <p>Movies Rs 200</p>
+        </>
+
+    
+    )
+}
+export default ExpenseItem;
