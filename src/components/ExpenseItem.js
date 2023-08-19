d88dcc81699ab23ab24e604c11bfaf6be29f91e5
neha89 <kamini.er@gmail.com>
2023-08-19 11:58:47 +0530

    Added one more variable to get location of expenditure

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,20 @@
-function ExpenseItem(){
-    return (
-        <>
-            <h2>Expense Items</h2>
-            <p>Food Rs 10</p>
-            <p>Petrol Rs 100</p>
-            <p>Movies Rs 200</p>
-        </>
+import './ExpenseItem.css';
 
-    
-    )
+function ExpenseItem(){
+
+    const expenseDate = new Date(2023, 8, 28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 500.5;
+    const locationOfExpenditure = 'Bangalore';
+    return (
+        <div className='expense-item'>
+            <div>{expenseDate.toISOString()}</div>
+            <div className='expense-item__description'>
+                <h2>{expenseTitle}</h2> 
+                <div className='expense-item__price'>${expenseAmount} </div>
+                <p>At {locationOfExpenditure}</p>
+            </div>
+        </div>
+    );
 }
 export default ExpenseItem;
