1735460ed7b1e918ed739790725f85b43f055cc6
neha89 <kamini.er@gmail.com>
2023-08-20 23:29:54 +0530

    locationOfExpenditure as a prop, loop in component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,14 @@
 import './ExpenseItem.css';
 
-function ExpenseItem(){
-
-    const expenseDate = new Date(2023, 8, 28);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 500.5;
-    const locationOfExpenditure = 'Bangalore';
+function ExpenseItem(props){
     return (
         <div className='expense-item'>
-            <div>{expenseDate.toISOString()}</div>
+            <div>{props.date.toISOString()}</div>
             <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2> 
-                <div className='expense-item__price'>${expenseAmount} </div>
-                <p>At {locationOfExpenditure}</p>
+                <h2>{props.title}</h2> 
+                <div className='expense-item__price'>${props.amount} </div>
+                <div>{props.location}</div>
+                
             </div>
         </div>
     );
