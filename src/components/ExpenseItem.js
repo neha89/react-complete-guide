99cbb78efcc8122fc2ca7a3735c0707034528bf7
neha89 <kamini.er@gmail.com>
2023-08-22 16:27:57 +0530

    added component ExpenseDetails has title,amount

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,17 @@
-import './ExpenseItem.css';
+import ExpenseDetails from "./ExpenseDetails";
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
 
-function ExpenseItem(props){
-    return (
-        <div className='expense-item'>
-            <div>{props.date.toISOString()}</div>
-            <div className='expense-item__description'>
-                <h2>{props.title}</h2> 
-                <div className='expense-item__price'>${props.amount} </div>
-                <div>{props.location}</div>
-                
-            </div>
-        </div>
-    );
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <ExpenseDetails
+        amount={props.amount}
+        location={props.location}
+        title={props.title}
+      />
+    </div>
+  );
 }
 export default ExpenseItem;
